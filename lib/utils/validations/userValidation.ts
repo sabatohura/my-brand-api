4ddc243ee11902e81a54c0eabb0944d7f86c6b2f
sabatohura <sabato@hura.one>
2024-02-16 06:15:18 +0200

    User register validation

--- lib/utils/validations/userValidation.ts
+++ lib/utils/validations/userValidation.ts
@@ -1,7 +1,25 @@
-import Joi from "joi";
+import * as Joi from "joi";
 
-const validateLogin = (login) => {
+const validateLogin = (login: { email: string; password: string }) => {
   const loginSchema = Joi.object({
-    email: Joi.String,
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).max(36).required(),
   });
+
+  return loginSchema.validate(login);
 };
+
+const userRegisterValidate = (userData: {
+  name: string;
+  password: string;
+  email: string;
+}) => {
+  const registerSchema = Joi.object({
+    name: Joi.string().min(6).max(40).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).max(36).required(),
+  });
+
+  return registerSchema.validate(userData);
+};
+export { validateLogin, userRegisterValidate };
