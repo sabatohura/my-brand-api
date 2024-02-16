1ca6c168e27ac6f7eb62db1d5a4010e8b3abdfb9
sabatohura <sabato@hura.one>
2024-02-16 06:23:04 +0200

    Create comment and query message validator

--- lib/utils/validations/query.ts
+++ lib/utils/validations/query.ts
@@ -1,16 +1,16 @@
 import * as Joi from "joi";
 const queryMessageValidate = (message: {
   name: string;
-  password: string;
   email: string;
+  message: string;
 }) => {
-  const registerSchema = Joi.object({
+  const messageSchema = Joi.object({
     name: Joi.string().min(6).max(40).required(),
     email: Joi.string().email().required(),
     message: Joi.string().min(20).max(500).required(),
   });
 
-  return registerSchema.validate(message);
+  return messageSchema.validate(message);
 };
 
 export default queryMessageValidate;
