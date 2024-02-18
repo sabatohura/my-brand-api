c137c46bdb305364d26b67c30b8ed5c5e26808da
sabatohura <sabato@hura.one>
2024-02-18 17:42:00 +0200

    Query message controllers and validator change

--- lib/controllers/message.ts
+++ lib/controllers/message.ts
@@ -1,1 +1,45 @@
+import { Request, Response } from "express";
+import { appMessage } from "./../models";
+import { contactMessageValidate } from "utils/validations";
 
+export const listQueryMessages = async (res: Response, req: Request) => {
+  const messages = await appMessage.find();
+  if (messages) {
+    res.status(200).send(messages);
+  } else {
+    res.status(200).send({ message: "you don't have any message" });
+  }
+};
+
+export const sendMessage = async (res: Response, req: Request) => {
+  try {
+    const valid = contactMessageValidate(req.body);
+    if (!valid.error) {
+      const message = new appMessage(req.body);
+      await message.save();
+      res.status(200).send(message);
+    } else {
+      res.status(400).send({ error: "Message fields can not be validated" });
+    }
+  } catch (error) {
+    res.status(400).send({ error: `could not send message due to ${error}` });
+  }
+};
+
+export const deleteMessage = async (res: Response, req: Request) => {
+  try {
+    await appMessage.deleteOne({ _id: req.params.id });
+    res.status(204).send({ message: "message deleted successfull" });
+  } catch {
+    res.status(404).send({ error: "message doesn't exist!" });
+  }
+};
+
+export const updateMessageStatus = async (res: Response, req: Request) => {
+  try {
+    const message = await appMessage.findOne({ _id: req.params.id });
+    message.status = "read";
+  } catch {
+    res.status(404).send({ error: "message status can't be changed" });
+  }
+};
--- lib/utils/validations/message.ts
+++ lib/utils/validations/message.ts
@@ -1,12 +1,12 @@
 import * as Joi from "joi";
 const queryMessageValidate = (message: {
-  name: string;
-  email: string;
+  senderName: string;
+  senderEmail: string;
   message: string;
 }) => {
   const messageSchema = Joi.object({
-    name: Joi.string().min(6).max(40).required(),
-    email: Joi.string().email().required(),
+    senderName: Joi.string().min(6).max(40).required(),
+    senderEmail: Joi.string().email().required(),
     message: Joi.string().min(20).max(500).required(),
   });
 
