f41b1790187490774d3d0f2c403b77a837b14823
sabatohura <sabato@hura.one>
2024-02-19 15:43:37 +0200

    Config middlewares

---
+++ lib/middlewares/admin.auth.ts
@@ -0,0 +1,26 @@
+import passport from "./../config/passport";
+import { Request, Response, NextFunction } from "express";
+
+const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate("jwt", { session: false }, (err, user) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorized. Please login to continue." });
+    }
+
+    if (user.role !== "Admin") {
+      return res.status(401).json({ error: "Unauthorized. for this action." });
+    }
+
+    req.user = user;
+    console.log(user);
+    return next();
+  })(req, res, next);
+};
+
+export default isAdmin;
